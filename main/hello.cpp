2bc17617d17d27803580f30fb5c38685c67c7e27
Varshagonal <143065231+Varshagonal@users.noreply.github.com>
2025-03-26 23:13:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main(void) {
+  cout<<"pes1ug22cs675\n";
+  cout<<"varsha\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
